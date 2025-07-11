cbd26911e0865e630115ab8d4cbe73d1d18c98bc
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-07-11 10:53:29 -0300

    fix(chat): Missing typing

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,12 +2,13 @@
 
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
+import { type CoreMessage } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages } = (await req.json()) as { messages: CoreMessage[] };
 
   const result = streamText({
     model: openai("gpt-4o"),
